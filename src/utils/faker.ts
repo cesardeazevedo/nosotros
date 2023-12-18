fe59abc06c9ed2c828d4fe8fcc80576cc3c36df0
Cesar Augusto <cesardeazevedo@outlook.com>
2023-12-18 16:47:04 +0900

    Cleanup on test helpers and utility functions

--- src/utils/faker.ts
+++ src/utils/faker.ts
@@ -1,15 +1,14 @@
 import { Kind } from 'constants/kinds'
-import { Event } from 'nostr-tools'
-import { UserState } from 'stores/nostr/user.store'
+import { Event, getEventHash, getPublicKey, getSignature } from 'nostr-tools'
+import { UserMetaData } from 'stores/modules/user.store'
 
-export function getImageUrl(width: number, height?: number) {
+export function fakeImageUrl(width: number, height?: number) {
   return `https://dummyimage.com/${width}x${height || width}/000/fff.jpg`
 }
 
-export function fakeUserData(data?: Partial<UserState>): UserState {
+export function fakeUserData(data?: Partial<UserMetaData>): UserMetaData {
   return {
     ...data,
-    id: data?.id || '1',
     name: data?.name || 'name',
     about: data?.about || 'user description',
     picture: data?.picture ?? 'https://dummyimage.com/120x120/000/fff',
@@ -20,7 +19,7 @@ export function fakeUserData(data?: Partial<UserState>): UserState {
   }
 }
 
-export function fakeUser(pubkey?: string, data?: Partial<UserState>): Event {
+export function fakeUser(pubkey?: string, data?: Partial<UserMetaData>): Event {
   return {
     kind: Kind.Metadata,
     id: '1',
@@ -34,13 +33,36 @@ export function fakeUser(pubkey?: string, data?: Partial<UserState>): Event {
 
 export function fakeNote(data?: Partial<Event>): Event {
   return {
-    kind: Kind.Text,
+    kind: data?.kind || Kind.Text,
     id: data?.id || Math.random().toString().slice(2),
     content: data?.content || 'Hello World',
     created_at: data?.created_at || Date.now() / 1000 - 1000,
     pubkey: data?.pubkey || '1',
     tags: data?.tags || [[]],
     sig: '',
+  }
+}
+
+// To help some tests that need signatures
+const privateKeys = {
+  '1': 'dc75513e6a77f59c06af06fc5cbc6e56e01aad1b53289ae04b73ffcaa479a629',
+  '2': '3f2ae5a00851c710a878fe71fdf3aabfde510e426f6f8b03f53fde213209579e',
+  '3': '9a36a7d13128752a183b4153bc5242605c44eabb6e7e72e6f4fdc83c51aaa786',
+  '4': 'cea609544ceca847d5b0053be42d236772c5041c4c651d2bc91c74149e8fc7f7',
+  '5': 'dd6b130dbbb576c0fc71b166522dcad04e003b74d88690898ce5c2b630ead6d8',
+} as Record<string, string>
+
+export function fakeSignature(wrappedEvent: Event) {
+  const { id, sig, ...rest } = wrappedEvent
+  const privateKey = privateKeys[rest.pubkey || '1'] || privateKeys['1']
+  const event = {
+    ...rest,
+    pubkey: getPublicKey(privateKey),
+  }
+  return {
+    ...event,
+    id: getEventHash(event),
+    sig: getSignature(event, privateKey),
   }
 }
 
--- src/utils/fixtures.ts
+++ src/utils/fixtures.ts
@@ -5,25 +5,25 @@ import { Filter, Options as FilterOptions } from 'stores/core/filter'
 import { Subscription, SubscriptionOptions } from 'stores/core/subscription'
 import { database } from 'stores/db/database.store'
 import { ObservableDB, dbBatcher } from 'stores/db/observabledb.store'
-import { FeedStore } from 'stores/modules/feed.store'
-import { PostStore } from 'stores/modules/post.store'
-import { RelayStore } from 'stores/nostr/userRelay.store'
+import { FeedOptions, FeedStore } from 'stores/modules/feed.store'
+import { Note } from 'stores/modules/note.store'
+import { SchemaNIP65 } from 'stores/nostr/userRelay.store'
 import { RootStore } from 'stores/root.store'
 import { fakeNote } from 'utils/faker'
-import { TestContext, test as base } from 'vitest'
+import { test as base } from 'vitest'
 
-export interface Fixtures {
+interface Fixtures {
   relays: WS[]
   root: RootStore
-  createFeed: () => FeedStore
-  createPost: (note: Partial<Event>) => PostStore
+  createFeed: (options?: FeedOptions) => FeedStore
+  createNote: (note: Partial<Event>) => Note
   createFilter: (filter: NostrFilter, options?: FilterOptions) => Filter
   createSubscription: (filters: NostrFilter, options?: SubscriptionOptions | undefined) => Subscription
-  createRelayList: (note: Partial<Event>) => Promise<RelayStore | undefined>
+  createRelayList: (note: Partial<Event>) => Promise<SchemaNIP65 | undefined>
   observabledb: ObservableDB<Record<string, unknown>>
 }
 
-export const defaultRelays = ['wss://relay1.com', 'wss://relay2.com', 'wss://relay3.com']
+const defaultRelays = ['wss://relay1.com', 'wss://relay2.com', 'wss://relay3.com']
 
 export const RELAY_1 = defaultRelays[0]
 export const RELAY_2 = defaultRelays[1]
@@ -46,32 +46,29 @@ export const test = base.extend<Fixtures>({
     await database.clear()
   },
 
-  createFeed: async ({ root }: TestContext, use) => {
-    await use(() => new FeedStore(root, { authors: ['1'], pagination: false }))
+  createFeed: async ({ root }, use) => {
+    await use((options?: FeedOptions) => new FeedStore(root, { authors: ['1'], pagination: false, ...options }))
   },
 
-  createPost: async ({ root }: TestContext, use) => {
-    await use((note) => new PostStore(root, fakeNote(note)))
-  },
-
-  createFilter: async ({ root }: TestContext, use: (fixture: Fixtures['createFilter']) => Promise<void>) => {
-    await use((filter, options) => new Filter(root, filter, options))
-  },
-
-  createSubscription: async (
-    { root }: TestContext,
-    // TS losing type information here for some reason
-    use: (fixture: Fixtures['createSubscription']) => Promise<void>,
-  ) => {
-    await use((filters, options): Subscription => {
-      return new Subscription(root, new Filter(root, filters), options)
+  createNote: async ({ root }, use) => {
+    await use((event: Partial<Event>) => {
+      const note = new Note(root, fakeNote(event))
+      note.imeta.parse()
+      note.content.parse()
+      return note
     })
   },
 
-  createRelayList: async ({ root }: TestContext, use) => {
-    await use(async (note) => {
-      return await root.userRelays.add(fakeNote(note))
-    })
+  createFilter: async ({ root }, use) => {
+    await use((filter: NostrFilter, options?: FilterOptions) => new Filter(root, filter, options))
+  },
+
+  createSubscription: async ({ root }, use) => {
+    await use((filters, options) => new Subscription(root, new Filter(root, filters), options))
+  },
+
+  createRelayList: async ({ root }, use) => {
+    await use(async (note) => await root.userRelays.add(fakeNote(note)))
     root.userRelays.relays.clear()
   },
 
@@ -81,5 +78,6 @@ export const test = base.extend<Fixtures>({
     await database.initialize()
     await use(db)
     db._data.clear()
+    await database.clear()
   },
 })
