5da3bd803dad39132faaa217fea31ca552192cad
Cesar Augusto <cesardeazevedo@outlook.com>
2023-12-18 16:01:36 +0900

    Moved recommended relays and pubkeys to env variables

--- src/constants/relays.ts
+++ src/constants/relays.ts
@@ -1,1 +1,2 @@
-export const FIXED_RELAYS = ['wss://purplepag.es']
+export const FIXED_RELAYS = JSON.parse(import.meta.env.VITE_FIXED_RELAYS)
+export const DEFAULT_RELAYS = JSON.parse(import.meta.env.VITE_DEFAULT_RELAYS)
