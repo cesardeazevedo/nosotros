25816df4bff9042adef32efbca2695f2c557d54c
Cesar Augusto <cesardeazevedo@outlook.com>
2024-07-12 14:21:00 +0800

    Refactor on signin components

---
+++ src/components/elements/SignIn/SignInHeader.tsx
@@ -0,0 +1,55 @@
+import { Box, Grow, IconButton, Typography } from "@mui/material"
+import { IconChevronLeft, IconX } from "@tabler/icons-react"
+import { useMobile } from "hooks/useMobile"
+import { useCallback } from "react"
+import { Row } from "../Layouts/Flex"
+import { OnboardMachineContext } from "./SignInContext"
+
+const easings = {
+  enter: 'cubic-bezier(0.33, 1, 0.68, 1)',
+  exit: 'cubic-bezier(0.33, 1, 0.68, 1)',
+}
+
+function SignInHeader() {
+  const machine = OnboardMachineContext.useActorRef()
+  const state = OnboardMachineContext.useSelector((x) => x)
+  const camera = OnboardMachineContext.useSelector((x) => x.context.camera)
+
+  const isMobile = useMobile()
+
+  const handleBack = useCallback(() => {
+    machine.send({ type: 'back' })
+  }, [machine])
+
+  const handleCamera = useCallback(() => {
+    //
+  }, [])
+
+  return (
+    <Row sx={{ p: 2, justifyContent: 'center', height: 64 }}>
+      <Box sx={{ width: 38, height: 38 }}>
+        <Grow in={!camera && !state.matches('intro')} easing={easings}>
+          <IconButton
+            size='small'
+            onClick={handleBack}
+            sx={{ position: 'absolute', left: 16, color: 'text.primary', transitionDuration: '250ms!important' }}>
+            <IconChevronLeft size={28} strokeWidth='2.5' />
+          </IconButton>
+        </Grow>
+        <Grow in={camera} easing={easings}>
+          <IconButton
+            size='small'
+            onClick={handleCamera}
+            sx={{ position: 'absolute', left: 16, color: 'text.primary', transitionDuration: '250ms!important' }}>
+            <IconX size={28} strokeWidth='2.0' />
+          </IconButton>
+        </Grow>
+      </Box>
+      <Typography variant='h6' sx={{ flex: 1, textAlign: 'center', mr: 6 }}>
+        {!isMobile ? 'Sign In' : camera ? 'Camera' : ''}
+      </Typography>
+    </Row >
+  )
+}
+
+export default SignInHeader
---
+++ src/machines/onboard.machine.ts
@@ -0,0 +1,112 @@
+import { appState } from 'stores/app.state'
+import { assign, fromPromise, setup } from 'xstate'
+
+const clipboard = fromPromise(async () => {
+  const permissionStatus = await navigator.permissions.query({ name: 'clipboard-read' as PermissionName })
+  if (permissionStatus.state === 'granted') {
+    const text = await window.navigator.clipboard.readText()
+    return { text }
+  }
+  return Promise.reject('Permission not granted')
+})
+
+type Context = {
+  pubkey?: string
+  clipboardError?: string
+  camera: boolean
+}
+
+export const onboardMachine = setup({
+  types: {
+    context: {} as Context,
+    meta: {} as { index: number },
+    events: {} as
+      | { type: 'back' }
+      | { type: 'next' }
+      | { type: 'next.pubkey' }
+      | { type: 'paste' }
+  },
+  actors: {
+    clipboard,
+  },
+  actions: {
+    subscribeUser({ context }) {
+      const { pubkey } = context
+      if (pubkey) {
+        appState.client.users.subscribe([pubkey]).subscribe()
+      }
+    }
+  }
+}).createMachine({
+  initial: 'intro',
+  context: {
+    pubkey: undefined,
+    clipboardError: undefined,
+    camera: false,
+  },
+  states: {
+    intro: {
+      meta: {
+        index: 0,
+      },
+      on: {
+        'next': {
+          target: 'withPubkey',
+        },
+        'back': {
+        },
+      }
+    },
+    withPubkey: {
+      meta: {
+        index: 1,
+      },
+      initial: 'idle',
+      states: {
+        idle: {
+          on: {
+            paste: {
+              target: 'clipboard'
+            }
+          }
+        },
+        clipboard: {
+          invoke: {
+            src: 'clipboard',
+            onError: {
+              target: 'clipboardRejected'
+            },
+            onDone: {
+              actions: assign((args) => ({ pubkey: args.event.output?.text })),
+              target: 'clipboardAccepted'
+            }
+          }
+        },
+        clipboardAccepted: {},
+        clipboardRejected: {
+          entry: [
+            assign(() => ({ clipboardError: 'Clipboard permission not granted' })),
+          ]
+        }
+      },
+      on: {
+        'next': {
+          target: 'confirm',
+        },
+        'back': {
+          target: 'intro',
+        }
+      }
+    },
+    withRemoteSigner: {
+      meta: {
+        index: 1,
+      },
+    },
+    confirm: {
+      meta: {
+        index: 2,
+      },
+    },
+  }
+})
