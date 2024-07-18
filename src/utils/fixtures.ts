2a2c53cd7e24081441ee0aa7ee45cc5b60306888
Cesar Augusto <cesardeazevedo@outlook.com>
2024-07-19 01:41:13 +0800

    Create a ws instance for each fixture

--- src/utils/fixtures.ts
+++ src/utils/fixtures.ts
@@ -24,28 +24,30 @@ export const RELAY_3 = defaultRelays[2]
 export const RELAY_4 = defaultRelays[3]
 export const RELAY_5 = defaultRelays[4]
 
-const relay3 = new WS(RELAY_3, { jsonProtocol: true })
-const relay4 = new WS(RELAY_4, { jsonProtocol: true })
-const relay5 = new WS(RELAY_5, { jsonProtocol: true })
-
 export const test = base.extend<Fixtures>({
-  relay: async ({ }, use) => {
+  relay: async ({}, use) => {
     const relay = new WS(RELAY_1, { jsonProtocol: true })
     await use(relay)
     relay.close()
   },
-  relay2: async ({ }, use) => {
+  relay2: async ({}, use) => {
     const relay = new WS(RELAY_2, { jsonProtocol: true })
     await use(relay)
     relay.close()
   },
-  relay3: async ({ }, use) => {
-    await use(relay3)
+  relay3: async ({}, use) => {
+    const relay = new WS(RELAY_3, { jsonProtocol: true })
+    await use(relay)
+    relay.close()
   },
-  relay4: async ({ }, use) => {
-    await use(relay4)
+  relay4: async ({}, use) => {
+    const relay = new WS(RELAY_4, { jsonProtocol: true })
+    await use(relay)
+    relay.close()
   },
-  relay5: async ({ }, use) => {
-    await use(relay5)
+  relay5: async ({}, use) => {
+    const relay = new WS(RELAY_5, { jsonProtocol: true })
+    await use(relay)
+    relay.close()
   },
 })
