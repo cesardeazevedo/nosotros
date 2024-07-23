74ebad02c0322381915f3305d310b3fd5f979973
Cesar Augusto <cesardeazevedo@outlook.com>
2024-07-23 14:53:29 +0800

    NIP05 now can be disabled

--- src/components/elements/Settings/SettingsNostr.tsx
+++ src/components/elements/Settings/SettingsNostr.tsx
@@ -1,13 +1,4 @@
-import {
-  Divider,
-  List,
-  ListItem,
-  ListItemIcon,
-  ListItemText,
-  ListSubheader,
-  Switch,
-  Typography,
-} from '@mui/material'
+import { Divider, List, ListItem, ListItemIcon, ListItemText, ListSubheader, Switch, Typography } from '@mui/material'
 import { IconBolt, IconHeart } from '@tabler/icons-react'
 import { observer } from 'mobx-react-lite'
 import { settingsStore } from 'stores/ui/settings.store'
@@ -21,21 +12,45 @@ const SettingsNostr = observer(function SettingsNostr() {
           primary={<Typography variant='subtitle1'>Outbox</Typography>}
           secondary='The outbox model helps you find notes from others people relays'
         />
-        <Switch edge='end' checked={settingsStore.nostrSettings.outboxEnabled} onClick={() => settingsStore.toggleSettings('outboxEnabled')} />
+        <Switch
+          edge='end'
+          checked={settingsStore.nostrSettings.outboxEnabled}
+          onClick={() => settingsStore.toggleSettings('outboxEnabled')}
+        />
       </ListItem>
       <ListItem dense sx={{ mb: 1 }}>
         <ListItemText primary={<Typography variant='subtitle1'>Relay Hints</Typography>} />
-        <Switch edge='end' checked={settingsStore.nostrSettings.hintsEnabled} onChange={() => settingsStore.toggleSettings('hintsEnabled')} />
+        <Switch
+          edge='end'
+          checked={settingsStore.nostrSettings.hintsEnabled}
+          onChange={() => settingsStore.toggleSettings('hintsEnabled')}
+        />
+      </ListItem>
+      <ListItem dense sx={{ mb: 1 }}>
+        <ListItemText primary={<Typography variant='subtitle1'>NIP05</Typography>} />
+        <Switch
+          edge='end'
+          checked={settingsStore.nostrSettings.nip05enabled}
+          onChange={() => settingsStore.toggleSettings('nip05enabled')}
+        />
       </ListItem>
       <Divider />
       <ListSubheader sx={{ mt: 2, lineHeight: 2 }}>UI</ListSubheader>
       <ListItem dense>
-        <ListItemIcon sx={{ minWidth: 32 }}><IconHeart strokeWidth='1.4' /></ListItemIcon>
+        <ListItemIcon sx={{ minWidth: 32 }}>
+          <IconHeart strokeWidth='1.4' />
+        </ListItemIcon>
         <ListItemText primary={<Typography variant='subtitle1'>Reactions</Typography>} />
-        <Switch edge='end' checked={settingsStore.nostrSettings.nip25enabled} onChange={() => settingsStore.toggleSettings('nip25enabled')} />
+        <Switch
+          edge='end'
+          checked={settingsStore.nostrSettings.nip25enabled}
+          onChange={() => settingsStore.toggleSettings('nip25enabled')}
+        />
       </ListItem>
       <ListItem dense>
-        <ListItemIcon sx={{ minWidth: 32 }}><IconBolt strokeWidth='1.4' /></ListItemIcon>
+        <ListItemIcon sx={{ minWidth: 32 }}>
+          <IconBolt strokeWidth='1.4' />
+        </ListItemIcon>
         <ListItemText primary={<Typography variant='subtitle1'>Zaps</Typography>} />
         <Switch edge='end' checked={settingsStore.nostrSettings.nip57enabled} />
       </ListItem>
