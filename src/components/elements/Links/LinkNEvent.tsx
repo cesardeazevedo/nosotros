c935d33234d9c9d7c3d38682ae9b1fe7ca3f4b0b
Cesar Augusto <cesardeazevedo@outlook.com>
2024-07-25 22:33:33 +0800

    Change links behavior when on deck route to add  a new column

--- src/components/elements/Links/LinkNEvent.tsx
+++ src/components/elements/Links/LinkNEvent.tsx
@@ -1,8 +1,11 @@
 import { type LinkProps } from '@mui/material'
+import { useMatch } from '@tanstack/react-router'
 import { observer } from 'mobx-react-lite'
-import React from 'react'
+import React, { useCallback, useContext } from 'react'
 import type Note from 'stores/models/note'
+import { deckStore } from 'stores/ui/deck.store'
 import LinkRouter from './LinkRouter'
+import { DeckContext } from '../Deck/DeckList'
 
 interface Props extends Omit<LinkProps, 'ref'> {
   note: Note
@@ -13,9 +16,26 @@ interface Props extends Omit<LinkProps, 'ref'> {
 
 const LinkNEvent = observer(function LinkNEvent(props: Props) {
   const { note, disableLink, ...rest } = props
+  const route = useMatch({ strict: false })
+  const isDeck = route.id === '/deck'
+  const { index } = useContext(DeckContext)
+
+  const handleClickDeck = useCallback(() => {
+    if (note) {
+      deckStore.addNoteColumn({ noteId: note.id }, index + 1)
+    }
+  }, [note, index])
 
   if (disableLink) {
     return props.children
+  }
+
+  if (isDeck) {
+    return (
+      <LinkRouter onClick={handleClickDeck} sx={{ cursor: 'pointer' }} {...rest}>
+        {props.children}
+      </LinkRouter>
+    )
   }
 
   return (
