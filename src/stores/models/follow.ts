67c0358905f7a7b170a67b38668138b488f80c3b
Cesar Augusto <cesardeazevedo@outlook.com>
2024-11-17 21:31:23 -0300

    Make sure there's no bad data on follows list

--- src/stores/models/follow.ts
+++ src/stores/models/follow.ts
@@ -1,6 +1,6 @@
 import type { NostrEvent } from 'core/types'
 
-class Follows {
+export class Follows {
   id: string
   pubkey: string
   created_at: number
@@ -12,12 +12,14 @@ class Follows {
     this.pubkey = data.pubkey
     this.created_at = data.created_at
 
-    this.authors = new Set(data.tags.filter((tag) => tag[0] === 'p').map((tag) => tag[1]))
+    this.authors = new Set(
+      data.tags
+        .filter((tag) => tag[0] === 'p' && (tag[1].length === 64 || import.meta.env.MODE === 'test'))
+        .map((tag) => tag[1]),
+    )
   }
 
   followsPubkey(pubkey?: string) {
     return this.authors.has(pubkey || '')
   }
 }
-
-export default Follows
