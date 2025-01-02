63fb4e68b115aaecdbb78450a7e2b9e7e60d4403
Cesar Augusto <cesardeazevedo@outlook.com>
2025-01-02 18:27:14 -0300

    Small adjustments on start() operator

--- src/core/operators/start.ts
+++ src/core/operators/start.ts
@@ -3,18 +3,16 @@ import type { NostrSubscription } from 'core/NostrSubscription'
 import type { Pool } from 'core/pool'
 import type { NostrEvent } from 'core/types'
 import type { OperatorFunction } from 'rxjs'
-import { EMPTY, identity, map, mergeMap, of } from 'rxjs'
+import { EMPTY, mergeMap, of } from 'rxjs'
 import { bufferTime } from './bufferTime'
 import { subscribe } from './subscribe'
 
 export function start(pool: Pool): OperatorFunction<NostrSubscription, [string, NostrEvent]> {
   return mergeMap((sub) => {
     return sub.relayFilters.pipe(
-      bufferTime(500),
+      bufferTime(700),
 
-      map((relayFilters) => mergeRelayFilters(relayFilters)),
-
-      mergeMap(identity),
+      mergeMap((relayFilters) => mergeRelayFilters(relayFilters)),
 
       mergeMap(([url, filters]) => {
         const relay = pool.get(url)
