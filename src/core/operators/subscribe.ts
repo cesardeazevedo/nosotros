4c684c54b5df868d52c48d6dc456c9bc1b7c0f34
Cesar Augusto <cesardeazevedo@outlook.com>
2025-01-02 18:27:48 -0300

    Add support for closeOnEose subscribe() operator
    * This will be necessary on nip46 signer and live subscriptions

--- src/core/operators/subscribe.ts
+++ src/core/operators/subscribe.ts
@@ -3,11 +3,12 @@ import type { Relay } from 'core/Relay'
 import type { MessageReceived, NostrEvent, NostrFilter } from 'core/types'
 import { ClientToRelay, RelayToClient } from 'core/types'
 import type { OperatorFunction } from 'rxjs'
-import { EMPTY, catchError, map, mergeMap, takeUntil, takeWhile, timer } from 'rxjs'
+import { EMPTY, catchError, filter, map, mergeMap, takeUntil, takeWhile, timer } from 'rxjs'
 
 export function subscribe(
   relay: Relay,
   filters?: NostrFilter[],
+  closeOnEose = true,
 ): OperatorFunction<NostrSubscription, [string, NostrEvent]> {
   return mergeMap((sub: NostrSubscription) => {
     const subMsg = () => [ClientToRelay.REQ, sub.id, ...(filters || sub.filters)]
@@ -15,16 +16,18 @@ export function subscribe(
     const messageFilter = (msg: MessageReceived) => msg[1] === sub.id
 
     return relay.websocket$.multiplex(subMsg, unsubMsg, messageFilter).pipe(
-      map((msg) => msg as unknown as MessageReceived),
       takeWhile((msg) => {
         const verb = msg[0].toLowerCase()
-        return verb !== RelayToClient.EOSE && verb !== RelayToClient.CLOSED
+        return (verb !== RelayToClient.EOSE && verb !== RelayToClient.CLOSED) || closeOnEose === false
       }),
+      // needed in case of closeOnEose false
+      filter(([verb]) => verb.toLowerCase() === RelayToClient.EVENT),
+
       map((msg) => [relay.url, msg[2]] as [string, NostrEvent]),
 
       catchError(() => EMPTY),
 
-      takeUntil(timer(5000)),
+      takeUntil(closeOnEose ? timer(5000) : EMPTY),
     )
   })
 }
