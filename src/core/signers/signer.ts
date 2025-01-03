68d55f1aedf5fd4e88f7878354bd07f0908cdc27
Cesar Augusto <cesardeazevedo@outlook.com>
2025-01-03 12:55:05 -0300

    Initial implementation of nip46 signer

--- src/core/signers/signer.ts
+++ src/core/signers/signer.ts
@@ -1,5 +1,9 @@
 import type { NostrEvent, UnsignedEvent } from 'nostr-tools'
 
-export interface Signer {
+export interface Signer<T = Record<string, unknown>> {
+  name: string
+  options?: T
   sign(event: UnsignedEvent): Promise<NostrEvent | false>
+  encrypt(pubkey: string, msg: string): Promise<string>
+  decrypt(pubkey: string, msg: string): Promise<string>
 }
