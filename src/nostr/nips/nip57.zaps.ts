ff80847a934beeb0abb1c077e93883110fdac256
Cesar Augusto <cesardeazevedo@outlook.com>
2025-01-13 23:15:33 -0300

    Fix zap response when waiting for receipt

--- src/nostr/nips/nip57.zaps.ts
+++ src/nostr/nips/nip57.zaps.ts
@@ -23,7 +23,7 @@ export class NIP57Zaps {
       filter((event) => {
         // Make sure the zap receipt is the one we are looking for
         const tags = parseTags(event.tags)
-        return tags.bolt11?.flat()[0] === invoice
+        return tags.bolt11?.[0][1] === invoice
       }),
     )
   }
