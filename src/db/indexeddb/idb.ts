14405bbbb03fd27a6830049f2cc773944432516b
Cesar Augusto <cesardeazevedo@outlook.com>
2025-01-14 03:23:46 -0300

    Initial settings storage page
    Need to come up with some dupe pie chart

--- src/db/indexeddb/idb.ts
+++ src/db/indexeddb/idb.ts
@@ -6,6 +6,7 @@ import { schemas, type IndexedDBSchema } from './idb.schemas'
 import { IDBMetadata } from './metadata/idb.metadata'
 import { IDBRelayStats } from './relayStats/idb.relayStats'
 import { IDBSeenStore } from './seen/idb.seen'
+import { IDBStats } from './stats/idb.stats'
 
 const DB_VERSION = 11
 
@@ -16,6 +17,7 @@ export class IDBStorage implements DB {
   event: IDBEventStore
   relayStats: IDBRelayStats
   metadata: IDBMetadata
+  stats: IDBStats
 
   constructor(name: string) {
     this.db = buildDB(name, DB_VERSION, schemas)
@@ -24,6 +26,49 @@ export class IDBStorage implements DB {
     this.event = new IDBEventStore(this.db)
     this.relayStats = new IDBRelayStats(this.db)
     this.metadata = new IDBMetadata(this.db)
+    this.stats = new IDBStats(this.db)
+  }
+
+  async export() {
+    const db = await this.db
+    const tx = db.transaction('events', 'readonly')
+    const events = tx.objectStore('events')
+    let csvContent = ''
+    const writer = new WritableStream({
+      write(chunk) {
+        csvContent += chunk
+      },
+    }).getWriter()
+
+    const data = await events.getAll()
+    for (const row of data) {
+      await writer.write(JSON.stringify(row) + '\n')
+    }
+    await writer.close()
+    const blob = new Blob([csvContent], { type: 'text/txt' })
+    const url = URL.createObjectURL(blob)
+    const a = document.createElement('a')
+    a.href = url
+    a.download = 'exported.txt'
+    document.body.appendChild(a)
+    a.click()
+    URL.revokeObjectURL(url)
+    a.remove()
+  }
+
+  async deleteMetadata() {
+    const db = await this.db
+    await db.clear('metadata')
+  }
+
+  async deleteEvents() {
+    const db = await this.db
+    await db.clear('events')
+  }
+
+  async deleteTags() {
+    const db = await this.db
+    await db.clear('tags')
   }
 
   async clearDB() {
