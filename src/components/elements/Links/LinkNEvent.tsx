ea2b8f13d0442c2a0b27f62cc6fb8d152bbadc40
Cesar Augusto <cesardeazevedo@outlook.com>
2025-01-16 20:26:23 -0300

    Always make sure to use nevent for links
    Some users might share a event with note1, and we certainly don't want
    that, this commit encodes the note1 into a nevent with the relays from
    the seen storage

--- src/components/elements/Links/LinkNEvent.tsx
+++ src/components/elements/Links/LinkNEvent.tsx
@@ -1,30 +1,56 @@
 import { useRootStore } from '@/hooks/useRootStore'
+import { seenStore } from '@/stores/seen/seen.store'
 import { decodeNIP19 } from '@/utils/nip19'
 import { Link, useRouter } from '@tanstack/react-router'
-import { observer } from 'mobx-react-lite'
-import type { NEvent } from 'nostr-tools/nip19'
+import { observer, useLocalObservable } from 'mobx-react-lite'
+import { nip19 } from 'nostr-tools'
+import type { NEvent, Note } from 'nostr-tools/nip19'
 import React, { useCallback, useContext } from 'react'
 import { css } from 'react-strict-dom'
 import { DeckContext } from '../Deck/DeckContext'
 
 export type Props = {
-  nevent?: NEvent
+  nevent?: NEvent | Note
   children: React.ReactNode
   underline?: boolean
   disableLink?: boolean
 }
 
 export const LinkNEvent = observer(function LinkNEvent(props: Props) {
-  const { nevent, disableLink, underline, ...rest } = props
+  const { disableLink, underline, ...rest } = props
   const router = useRouter()
   const root = useRootStore()
   const { index } = useContext(DeckContext)
 
+  // Some quotes might be note1
+  const nevent = useLocalObservable(() => ({
+    get value() {
+      if (props.nevent) {
+        const decoded = decodeNIP19(props.nevent)
+        if (decoded?.type === 'note') {
+          return nip19.neventEncode({
+            id: decoded.data,
+            relays: seenStore.get(decoded.data),
+          })
+        }
+      }
+      return props.nevent
+    },
+  })).value
+
   const handleClickDeck = useCallback(() => {
     if (nevent) {
       const decoded = decodeNIP19(nevent)
-      if (decoded?.type === 'nevent') {
-        root.decks.selected.addNEvent({ options: decoded.data }, (index || 0) + 1)
+      switch (decoded?.type) {
+        case 'nevent': {
+          root.decks.selected.addNEvent({ options: decoded.data }, (index || 0) + 1)
+          break
+        }
+        case 'note': {
+          const options = { id: decoded.data, relays: seenStore.get(decoded.data) }
+          root.decks.selected.addNEvent({ options }, (index || 0) + 1)
+          break
+        }
       }
     }
   }, [nevent, index])
