dd745f98989acccf35a62453011d17b18a188024
Cesar Augusto <cesardeazevedo@outlook.com>
2025-01-20 23:51:33 -0300

    Mailbox tracker needs shareReplay
    Needed because we are adding another pipe

--- src/nostr/mailbox.ts
+++ src/nostr/mailbox.ts
@@ -1,7 +1,7 @@
 import { Kind } from '@/constants/kinds'
 import { NostrSubscription } from '@/core/NostrSubscription'
 import type { NostrEvent } from 'nostr-tools'
-import { filter, from, map, mergeWith, pipe, Subject, take, takeUntil, tap, timer } from 'rxjs'
+import { filter, from, map, mergeWith, pipe, shareReplay, Subject, take, takeUntil, timer } from 'rxjs'
 import type { UserRelay } from './helpers/parseRelayList'
 import { parseRelayList } from './helpers/parseRelayList'
 import type { RelaySelectionConfig } from './helpers/selectRelays'
@@ -20,7 +20,7 @@ export class Mailbox {
   constructor(private client: NostrClient) {}
 
   private subscribe = replay.wrap((pubkey: string) => {
-    const sub = new NostrSubscription({ kinds: [Kind.RelayList], authors: [pubkey], limit: 1 })
+    const sub = new NostrSubscription({ kinds: [Kind.RelayList], authors: [pubkey] })
     return from(this.client.query(sub)).pipe(
       map((event) => parseRelayList(event).relayList),
       filter((x) => x.length > 0),
@@ -37,6 +37,7 @@ export class Mailbox {
         ),
       ),
       take(1),
+      shareReplay(1),
     )
     return stream.pipe(map((data) => selectRelays(data, config)))
   }
