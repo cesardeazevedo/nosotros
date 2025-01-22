254411c1cdf04835533daaf0518857246b4cef5d
Cesar Augusto <cesardeazevedo@outlook.com>
2025-01-22 06:06:15 -0300

    Flatten query filters so we use a limit
    Not being used yet, but will soon

--- src/nostr/operators/localDB.ts
+++ src/nostr/operators/localDB.ts
@@ -2,12 +2,20 @@ import type { NostrFilter } from 'core/types'
 import type { NostrEvent } from 'nostr-tools'
 import { isReplaceableKind } from 'nostr-tools/kinds'
 import type { OperatorFunction } from 'rxjs'
-import { concatMap, EMPTY, filter, from, mergeAll, of, tap } from 'rxjs'
+import { concatMap, EMPTY, filter, from, mergeMap, of, take, tap } from 'rxjs'
 import { db } from '../db'
 
 export function query(filters: NostrFilter[]) {
   if (filters.length > 0) {
-    return from(db.event.query(filters)).pipe(mergeAll())
+    return from(filters).pipe(
+      mergeMap((filter) => {
+        const query = db.event.query(filter)
+        if (filter.limit) {
+          return from(query).pipe(take(filter.limit))
+        }
+        return from(query)
+      }),
+    )
   }
   return EMPTY
 }
