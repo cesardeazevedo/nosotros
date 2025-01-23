fa7329af38892fded84fddae9ce84d8e49f1b971
Cesar Augusto <cesardeazevedo@outlook.com>
2025-01-22 22:35:14 -0300

    Make sure to get all monitor users on their own relays

--- src/nostr/subscriptions/withRelatedAuthor.ts
+++ src/nostr/subscriptions/withRelatedAuthor.ts
@@ -1,18 +1,18 @@
 import type { Observable } from 'rxjs'
 import { connect, from, ignoreElements, merge, mergeMap } from 'rxjs'
 import { isAuthorTag } from '../helpers/parseTags'
-import type { NostrClient } from '../nostr'
+import type { ClientSubOptions, NostrClient } from '../nostr'
 import type { NostrEventMetadata } from '../types'
 
 // Generic authors for any event
-export function withRelatedAuthors(client: NostrClient) {
+export function withRelatedAuthors(client: NostrClient, options?: ClientSubOptions) {
   return connect((shared$: Observable<NostrEventMetadata>) => {
     return merge(
       shared$,
       shared$.pipe(
         mergeMap((event) => {
           const authors = [event.pubkey, ...event.tags.filter(isAuthorTag).map((x) => x[1])]
-          return from(authors).pipe(mergeMap((pubkey) => client.users.subscribe(pubkey)))
+          return from(authors).pipe(mergeMap((pubkey) => client.users.subscribe(pubkey, options)))
         }),
         ignoreElements(),
       ),
