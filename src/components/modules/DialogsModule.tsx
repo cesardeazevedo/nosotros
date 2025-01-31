4fbfe5e27d3944397559d46546647c2f68776863
Cesar Augusto <cesardeazevedo@outlook.com>
2025-01-31 20:54:20 -0300

    Removed replies dialog
    Although that component was really nice specially on mobile, I don't
    want to maintain it anymore, people are also very use it an instant
    page transition than a fancy bottom sheet

--- src/components/modules/DialogsModule.tsx
+++ src/components/modules/DialogsModule.tsx
@@ -1,7 +1,6 @@
 import { CameraDialog } from 'components/dialogs/camera.dialog'
 import { ImagesDialog } from 'components/dialogs/images.dialog'
 import { QRCodeDialog } from 'components/dialogs/qrcode.dialog'
-import { RepliesDialog } from 'components/dialogs/replies.dialog'
 import { SignInDialog } from 'components/dialogs/signin.dialog'
 import React from 'react'
 import { EditorDialog } from '../dialogs/editor.dialog'
@@ -10,6 +9,7 @@ import { EditorReplyDialog } from '../dialogs/editor.reply.dialog'
 import { NoteStatsDialog } from '../dialogs/notestats.dialog'
 import { ZapDialog } from '../dialogs/zap.dialog'
 import { ZapRequestInvoiceDialog } from '../dialogs/zapinvoice.dialog'
+import { SignUpResponseDialog } from '../dialogs/signup.response.dialog'
 
 export const Dialogs = React.memo(function Dialogs() {
   return (
@@ -17,8 +17,8 @@ export const Dialogs = React.memo(function Dialogs() {
       <SignInDialog />
       <CameraDialog />
       <ImagesDialog />
-      <RepliesDialog />
       <QRCodeDialog />
+      <SignUpResponseDialog />
       <ZapDialog />
       <ZapRequestInvoiceDialog />
       <EditorDialog />
