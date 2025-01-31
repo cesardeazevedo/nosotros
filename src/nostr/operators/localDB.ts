54929904b306213a79c761711a8eaa98d97aa678
Cesar Augusto <cesardeazevedo@outlook.com>
2025-01-31 19:50:48 -0300

    Insert non-replaceable events in batch

--- src/nostr/operators/localDB.ts
+++ src/nostr/operators/localDB.ts
@@ -1,8 +1,8 @@
 import type { NostrFilter } from 'core/types'
 import type { NostrEvent } from 'nostr-tools'
-import { isReplaceableKind } from 'nostr-tools/kinds'
+import { isParameterizedReplaceableKind, isReplaceableKind } from 'nostr-tools/kinds'
 import type { OperatorFunction } from 'rxjs'
-import { concatMap, EMPTY, filter, from, mergeMap, of, take, tap } from 'rxjs'
+import { bufferTime, concatMap, EMPTY, filter, from, mergeMap, of, Subject, take, tap } from 'rxjs'
 import { db } from '../db'
 
 export function query(filters: NostrFilter[]) {
@@ -13,22 +13,31 @@ export function query(filters: NostrFilter[]) {
         if (filter.limit) {
           return from(query).pipe(take(filter.limit))
         }
-        return from(query)
+        return from(query).pipe()
       }),
     )
   }
   return EMPTY
 }
 
+const insertBatch = new Subject<NostrEvent>()
+insertBatch
+  .pipe(
+    bufferTime(4500),
+    filter((x) => x.length > 0),
+    tap((events) => db.event.insertBatch(events)),
+  )
+  .subscribe()
+
 export function insertEvent<T extends NostrEvent>(): OperatorFunction<T, T> {
   return concatMap((event) => {
-    if (isReplaceableKind(event.kind)) {
+    if (isReplaceableKind(event.kind) || isParameterizedReplaceableKind(event.kind)) {
       return of(event).pipe(
         concatMap((event) => db.event.insert(event)),
         // Filter out events that weren't inserted
         filter((x): x is T => !!x),
       )
     }
-    return of(event).pipe(tap((event) => db.event.insert(event)))
+    return of(event).pipe(tap((event) => insertBatch.next(event)))
   })
 }
