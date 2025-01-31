67d471178a95bedf08c4cd7ec80bdd56a093933c
Cesar Augusto <cesardeazevedo@outlook.com>
2025-01-31 19:41:02 -0300

    Added an extra worker when verifying events

--- src/core/workers/verify.worker.ts
+++ src/core/workers/verify.worker.ts
@@ -1,6 +1,6 @@
 import type { NostrEvent } from 'nostr-tools'
 import { verifyEvent } from 'nostr-tools'
 
-onmessage = function onMessageVerifyEvent(event: MessageEvent<NostrEvent>) {
+self.onmessage = function onMessageVerifyEvent(event: MessageEvent<NostrEvent>) {
   self.postMessage(verifyEvent(event.data))
 }
