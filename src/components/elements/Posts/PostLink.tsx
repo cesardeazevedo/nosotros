35c667d4498fa95283eb9bbff07700993adfd85f
Cesar Augusto <cesardeazevedo@outlook.com>
2025-02-04 16:14:43 -0300

    Don't use actual links on postLink on mobile

--- src/components/elements/Posts/PostLink.tsx
+++ src/components/elements/Posts/PostLink.tsx
@@ -2,13 +2,12 @@ import { useMobile } from '@/hooks/useMobile'
 import { subscribeNoteStats } from '@/nostr/subscriptions/subscribeNoteStats'
 import { useNostrClientContext } from '@/stores/context/nostr.context.hooks'
 import type { Note } from '@/stores/notes/note'
-import { useMatch } from '@tanstack/react-router'
+import { useMatch, useNavigate, useRouter } from '@tanstack/react-router'
 import { observer } from 'mobx-react-lite'
+import type { NAddr, NEvent } from 'nostr-tools/nip19'
 import { useCallback, useRef, type ReactNode } from 'react'
 import { css, html } from 'react-strict-dom'
 import type { StrictClickEvent } from 'react-strict-dom/dist/types/StrictReactDOMProps'
-import { LinkNAddress } from '../Links/LinkNAddress'
-import { LinkNEvent } from '../Links/LinkNEvent'
 
 type Props = {
   note: Note
@@ -20,6 +19,9 @@ export const PostLink = observer(function postList(props: Props) {
   const { note, children } = props
   const ref = useRef<HTMLDivElement>(null)
   const context = useMatch({ from: '/$nostr', shouldThrow: false })?.context
+  const mobile = useMobile()
+  const router = useRouter()
+  const navigate = useNavigate()
   const { client } = useNostrClientContext()
   const isCurrentNote = context?.decoded?.type === 'nevent' ? context?.decoded.data.id === note.id : false
   const isActive = isCurrentNote || note.repliesOpen === true
@@ -28,35 +30,31 @@ export const PostLink = observer(function postList(props: Props) {
     (e: StrictClickEvent) => {
       e.preventDefault()
       e.stopPropagation()
-      note.toggleContent(true)
-      note.toggleReplies(true)
-      note.setRepliesStatus('LOADING')
-      subscribeNoteStats(client, note.event.event, {}).subscribe({
-        complete: () => {
-          note.setRepliesStatus('LOADED')
-        },
-      })
+      if (!mobile) {
+        note.toggleContent(true)
+        note.toggleReplies(true)
+        note.setRepliesStatus('LOADING')
+        subscribeNoteStats(client, note.event.event, {}).subscribe({
+          complete: () => {
+            note.setRepliesStatus('LOADED')
+          },
+        })
+      } else {
+        navigate({
+          to: '/$nostr',
+          params: { nostr: note.event.isAddressable ? (note.event.naddress as NAddr) : (note.event.nevent as NEvent) },
+          state: { from: router.latestLocation.pathname } as never,
+        })
+      }
     },
     [note],
   )
 
-  const mobile = useMobile()
-  if (mobile && !isCurrentNote) {
-    if (note.event.isAddressable) {
-      return <LinkNAddress naddress={note.event.naddress}>{children}</LinkNAddress>
-    } else {
-      return <LinkNEvent nevent={note.event.nevent}>{children}</LinkNEvent>
-    }
-  } else {
-    return (
-      <html.div
-        ref={ref}
-        onClick={!isActive ? handleClick : undefined}
-        style={[styles.root, !isActive && styles.action]}>
-        {children}
-      </html.div>
-    )
-  }
+  return (
+    <html.div ref={ref} onClick={!isActive ? handleClick : undefined} style={[styles.root, !isActive && styles.action]}>
+      {children}
+    </html.div>
+  )
 })
 
 const styles = css.create({
