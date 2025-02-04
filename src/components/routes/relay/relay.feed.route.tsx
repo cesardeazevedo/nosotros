d6c76fd521671c9d774090f13272f1cc718afeb2
Cesar Augusto <cesardeazevedo@outlook.com>
2025-02-03 21:11:09 -0300

    Initial implementation of relay authentication for relay feeds
    Only for relay feeds for now

--- src/components/routes/relay/relay.feed.route.tsx
+++ src/components/routes/relay/relay.feed.route.tsx
@@ -1,18 +1,16 @@
 import { NostrEventFeedItem } from '@/components/elements/Event/NostrEventFeedItem'
+import { FeedList } from '@/components/elements/Feed/FeedList'
 import { FeedSettings } from '@/components/elements/Feed/FeedSettings'
-import { List } from '@/components/elements/List/List'
 import { PostLoading } from '@/components/elements/Posts/PostLoading'
 import { RelayChip } from '@/components/elements/Relays/RelayChip'
 import { ContentProvider } from '@/components/providers/ContentProvider'
-import { NostrProvider } from '@/components/providers/NostrProvider'
 import { Button } from '@/components/ui/Button/Button'
 import { Divider } from '@/components/ui/Divider/Divider'
 import { Expandable } from '@/components/ui/Expandable/Expandable'
 import { Stack } from '@/components/ui/Stack/Stack'
-import { Kind } from '@/constants/kinds'
 import { useFeedSubscription } from '@/hooks/useFeedSubscription'
-import { useGlobalNostrSettings } from '@/hooks/useRootStore'
-import { RelayFeedModuleModel } from '@/stores/relays/relay.feed.module'
+import { useNostrContextInitializer } from '@/stores/context/nostr.context.hooks'
+import { createRelayFeedModule } from '@/stores/relays/relay.feed.module'
 import { spacing } from '@/themes/spacing.stylex'
 import { IconChevronDown, IconChevronUp } from '@tabler/icons-react'
 import { useSearch } from '@tanstack/react-router'
@@ -26,74 +24,44 @@ export const RelayFeedRoute = observer(function RelayFeedRoute() {
   const params = useSearch({ from: '/' })
 
   const relays = params.relay ? [params.relay].flat() : []
-  const nostrSettings = useGlobalNostrSettings()
-  const [module] = useState(
-    RelayFeedModuleModel.create({
-      relays,
-      feed: {
-        filter: { kinds: [Kind.Text, Kind.Repost] },
-        scope: 'self',
-        blured: true,
-        options: {
-          includeParents: true,
-          includeReplies: true,
-        },
-      },
-      context: {
-        settings: {
-          ...nostrSettings,
-          localDB: false,
-          localRelays: [],
-          scroll: {
-            replies: true,
-            zaps: true,
-            reposts: true,
-            reactions: true,
-          },
-        },
-        options: {
-          relays,
-        },
-      },
-    }),
-  )
+
+  const [module] = useState(createRelayFeedModule(relays))
 
   const [expanded, setExpanded] = useState(false)
 
+  useNostrContextInitializer(module.context)
   useFeedSubscription(module.feed, module.contextWithFallback.client)
 
   return (
-    <NostrProvider nostrContext={() => module.context!}>
-      <CenteredContainer margin>
-        <PaperContainer elevation={1}>
-          <Stack sx={styles.header} justify='space-between'>
-            <Stack gap={0.5}>
-              {relays.map((relay) => (
-                <RelayChip key={relay} url={relay} />
-              ))}
-            </Stack>
-            <Button variant='filledTonal' onClick={() => setExpanded((prev) => !prev)}>
-              <Stack gap={0.5}>
-                {expanded ? <IconChevronUp size={18} /> : <IconChevronDown size={18} />}
-                Feed Settings
-              </Stack>
-            </Button>
+    <CenteredContainer margin>
+      <PaperContainer>
+        <Stack sx={styles.header} justify='space-between'>
+          <Stack gap={0.5}>
+            {relays.map((relay) => (
+              <RelayChip key={relay} url={relay} />
+            ))}
           </Stack>
-          <Expandable expanded={expanded}>
-            <FeedSettings feed={module.feed} />
-          </Expandable>
-          <Divider />
-          <ContentProvider value={{ blured: module.feed.blured }}>
-            <List
-              feed={module.feed}
-              onScrollEnd={module.feed.paginate}
-              render={(event) => <NostrEventFeedItem event={event} />}
-              footer={<PostLoading rows={4} />}
-            />
-          </ContentProvider>
-        </PaperContainer>
-      </CenteredContainer>
-    </NostrProvider>
+          <Button variant='filledTonal' onClick={() => setExpanded((prev) => !prev)}>
+            <Stack gap={0.5}>
+              {expanded ? <IconChevronUp size={18} /> : <IconChevronDown size={18} />}
+              Feed Settings
+            </Stack>
+          </Button>
+        </Stack>
+        <Expandable expanded={expanded}>
+          <FeedSettings feed={module.feed} />
+        </Expandable>
+        <Divider />
+        <ContentProvider value={{ blured: module.feed.blured }}>
+          <FeedList
+            feed={module.feed}
+            onScrollEnd={module.feed.paginate}
+            render={(event) => <NostrEventFeedItem event={event} />}
+            footer={<PostLoading rows={4} />}
+          />
+        </ContentProvider>
+      </PaperContainer>
+    </CenteredContainer>
   )
 })
 
--- src/stores/relays/relay.feed.module.ts
+++ src/stores/relays/relay.feed.module.ts
@@ -1,21 +1,77 @@
-import type { NostrClient } from '@/nostr/nostr'
+import { Kind } from '@/constants/kinds'
+import { authenticate } from '@/nostr/auth'
 import type { Instance, SnapshotIn, SnapshotOut } from 'mobx-state-tree'
 import { t } from 'mobx-state-tree'
+import { identity } from 'observable-hooks'
+import { distinct, mergeMap } from 'rxjs'
 import { NotesFeedSubscriptionModel } from '../feeds/feed.notes'
 import { FeedPaginationLimit } from '../feeds/feed.pagination.limit'
+import { toStream } from '../helpers/toStream'
 import { BaseModuleModel } from '../modules/module'
+import { rootStore } from '../root.store'
+import { relaysStore } from './relays.store'
 
 export const RelayFeedModuleModel = BaseModuleModel.named('RelayFeedModuleModel')
   .props({
-    type: t.optional(t.literal('nevent'), 'nevent'),
+    type: t.optional(t.literal('relayfeed'), 'relayfeed'),
     relays: t.array(t.string),
     feed: NotesFeedSubscriptionModel(FeedPaginationLimit),
   })
   .views((self) => ({
-    subscribe(client: NostrClient) {
-      return self.feed.subscribe(client)
+    get auths(): [string, string][] {
+      return [...relaysStore.auths.entries().filter(([relay]) => self.relays.includes(relay))]
     },
   }))
+  .actions((self) => ({
+    afterCreate() {
+      toStream(() => self.auths)
+        .pipe(
+          mergeMap(identity),
+          distinct((value) => value[0] + value[1]),
+          // Authenticate
+          mergeMap(([relay, challenge]) => authenticate(rootStore.rootContext.client, relay, challenge)),
+          // Restart the feed
+          mergeMap(() => self.feed.subscribe(self.context!.client)),
+        )
+        .subscribe()
+    },
+  }))
+
+export function createRelayFeedModule(relays: string[]) {
+  return RelayFeedModuleModel.create({
+    relays,
+    feed: {
+      limit: 100,
+      filter: { kinds: [Kind.Text, Kind.Repost] },
+      scope: 'self',
+      blured: true,
+      options: {
+        queryLocal: false,
+        includeParents: true,
+        includeReplies: true,
+      },
+    },
+    context: {
+      settings: {
+        ...rootStore.nostrSettings,
+        scroll: {
+          zaps: true,
+          replies: true,
+          reposts: true,
+          reactions: true,
+        },
+        nip05: false,
+        hints: true,
+        outbox: false,
+        localDB: false,
+        localRelays: [],
+      },
+      options: {
+        relays,
+      },
+    },
+  })
+}
 
 export interface RelayFeedModule extends Instance<typeof RelayFeedModuleModel> {}
 export interface RelayFeedModuleSnapshotIn extends SnapshotIn<typeof RelayFeedModuleModel> {}
