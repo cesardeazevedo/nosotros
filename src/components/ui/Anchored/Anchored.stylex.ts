8dedf6456cfe242afd6ef447759d6e6934b962a1
Cesar Augusto <cesardeazevedo@outlook.com>
2025-02-26 22:03:42 -0300

    Implemented Badge and Anchored components

---
+++ src/components/ui/Anchored/Anchored.stylex.ts
@@ -0,0 +1,12 @@
+import { css } from 'react-strict-dom'
+
+const vars = {
+  offset: '0',
+  translateX: '0',
+  translateY: '0',
+  originX: '0',
+  originY: '0',
+  scale: 1,
+}
+
+export const anchoredTokens = css.defineVars(vars)
