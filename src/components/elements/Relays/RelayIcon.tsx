1275f896e9f74c009c6f488d552b91053eeaee3f
Cesar Augusto <cesardeazevedo@outlook.com>
2025-04-14 14:52:02 -0300

    Lots of improvements on Relay components

---
+++ src/components/elements/Relays/RelayIcon.tsx
@@ -0,0 +1,29 @@
+import type { Props as AvatarProps } from '@/components/ui/Avatar/Avatar'
+import { Avatar } from '@/components/ui/Avatar/Avatar'
+import type { SxProps } from '@/components/ui/types'
+import { relaysStore } from '@/stores/relays/relays.store'
+import { observer } from 'mobx-react-lite'
+import { UserAvatar } from '../User/UserAvatar'
+
+type Props = {
+  size?: AvatarProps['size']
+  url?: string
+  onlyRelayIcon?: boolean
+  sx?: SxProps
+}
+
+export const RelayIcon = observer(function RelayIcon(props: Props) {
+  const { url, size = 'sm', onlyRelayIcon = false, sx } = props
+  const info = relaysStore.getInfo(url)
+  return (
+    <>
+      {info?.icon ? (
+        <Avatar size={size} src={info?.icon} sx={sx} />
+      ) : info?.pubkey && !onlyRelayIcon ? (
+        <UserAvatar size={size} pubkey={info?.pubkey} sx={sx} />
+      ) : (
+        <></>
+      )}
+    </>
+  )
+})
