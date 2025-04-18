4f083ee46f47b4f835f6ea909e185f2b60230fcb
Cesar Augusto <cesardeazevedo@outlook.com>
2025-04-18 04:31:45 -0300

    Fixes on FeedNewPosts component indicator
    The whole thing wasn't working well on deck

--- src/components/modules/Feed/FeedNewPosts.tsx
+++ src/components/modules/Feed/FeedNewPosts.tsx
@@ -11,33 +11,40 @@ import { spacing } from '@/themes/spacing.stylex'
 import { IconArrowUp } from '@tabler/icons-react'
 import { observer } from 'mobx-react-lite'
 import { useObservableRef, useObservableState } from 'observable-hooks'
-import type { RefObject } from 'react'
-import { css } from 'react-strict-dom'
-import { delay, filter, fromEvent, map, mergeMap, of, startWith, switchMap } from 'rxjs'
+import { useContext, type RefObject } from 'react'
+import { css, html } from 'react-strict-dom'
+import { delay, fromEvent, map, mergeMap, of, startWith, switchMap } from 'rxjs'
+import { DeckContext } from '../Deck/DeckContext'
 
 type Props = {
   feed: FeedStore
-  ref: RefObject<HTMLElement | null>
+
+  ref?: RefObject<HTMLElement | null>
 }
 
 export const FeedNewPosts = observer(function FeedNewPosts(props: Props) {
   const { feed } = props
+  const isDeck = useContext(DeckContext).index !== undefined
   const [, ref$] = useObservableRef(props.ref)
   const [fabVisible] = useObservableState(() => {
     return ref$.pipe(
-      map((ref) => ref.current),
-      filter((x) => !!x),
-      mergeMap((element) => fromEvent(element, 'scroll').pipe(map(() => element))),
-      map((x) => x.scrollTop > 250),
+      mergeMap((ref) => {
+        const element = ref?.current
+        if (element) {
+          return fromEvent(element, 'scroll').pipe(map(() => element.scrollTop))
+        }
+        return fromEvent(window, 'scroll').pipe(map(() => window.scrollY))
+      }),
+      map((position) => position > 250),
       switchMap((isVisible) => {
-        return of(isVisible).pipe(delay(isVisible ? 7500 : 0))
+        return of(isVisible).pipe(delay(isVisible ? 7000 : 0))
       }),
       startWith(false),
     )
   }, false)
 
   const handleFlush = () => {
-    if (props.ref.current) {
+    if (props.ref?.current) {
       props.ref.current.scrollTo({ top: 0, behavior: 'smooth' })
     }
     feed.flush()
@@ -62,21 +69,23 @@ export const FeedNewPosts = observer(function FeedNewPosts(props: Props) {
           <Divider />
         </>
       </Expandable>
-      <Fab
-        size='sm'
-        variant='primary'
-        sx={[styles.fab, fabVisible && feed.buffer.size > 0 && styles.fab$visible]}
-        onClick={handleFlush}
-        label={
-          <Stack gap={2}>
-            <Stack gap={1}>
-              <IconArrowUp size={20} />
-              {feed.buffer.size || ''} new notes
+      <html.div style={isDeck ? styles.floating$deck : styles.floating}>
+        <Fab
+          size='sm'
+          variant='primary'
+          sx={[styles.fab, fabVisible && feed.buffer.size > 0 && styles.fab$visible]}
+          onClick={handleFlush}
+          label={
+            <Stack gap={2}>
+              <Stack gap={1}>
+                <IconArrowUp size={20} />
+                {feed.buffer.size || ''} new notes
+              </Stack>
+              <UsersAvatars max={3} borderColor='primary' renderTooltip={false} pubkeys={feed.bufferPubkeys} />
             </Stack>
-            <UsersAvatars max={3} borderColor='primary' renderTooltip={false} pubkeys={feed.bufferPubkeys} />
-          </Stack>
-        }
-      />
+          }
+        />
+      </html.div>
     </>
   )
 })
@@ -88,23 +97,46 @@ const styles = css.create({
   button: {
     overflow: 'visible',
   },
+  floating: {
+    position: 'fixed',
+    top: 32,
+    textAlign: 'center',
+    display: 'flex',
+    alignSelf: 'center',
+    zIndex: 100,
+    height: 0,
+  },
+  floating$deck: {
+    position: 'sticky',
+    margin: 'auto',
+    textAlign: 'center',
+    top: 32,
+    left: 0,
+    right: 0,
+    height: 0,
+    zIndex: 100,
+  },
   fab: {
     display: 'flex',
-    position: 'fixed',
+    position: 'relative',
     alignSelf: 'center',
     borderRadius: shape.full,
-    top: 20,
+    left: 0,
+    right: 0,
+    top: 0,
     height: 40,
     padding: spacing.padding1,
     width: 'fit-content',
-    zIndex: 300,
     margin: 'auto',
+    zIndex: 400,
     opacity: 0,
+    pointerEvents: 'none',
     transitionDuration: duration.short3,
     transitionProperty: 'transform, opacity',
   },
   fab$visible: {
     opacity: 1,
+    pointerEvents: 'auto',
     transform: 'translateY(8px)',
   },
 })
