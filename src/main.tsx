b3d45215fc13ae62ca5362397c0146ca5d3ed468
Cesar Augusto <cesardeazevedo@outlook.com>
2025-04-18 21:00:56 -0300

    New service worker config

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,14 @@
 import { RouterProvider } from '@tanstack/react-router'
 import ReactDOM from 'react-dom/client'
+import { registerSW } from 'virtual:pwa-register'
 import { ContentProvider } from './components/providers/ContentProvider'
 import { RootStoreProvider } from './components/providers/RootStoreProvider'
 import { StylexProvider } from './components/providers/StylexProvider'
 import { router } from './Router'
 import { rootStore } from './stores/root.store'
 import './styles/stylex.css'
+
+registerSW({ immediate: true })
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <StylexProvider>
