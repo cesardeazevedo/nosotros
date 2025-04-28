90d3337e5bd1b1373e8cf28629028a5155988fb4
Cesar Augusto <cesardeazevedo@outlook.com>
2025-04-28 14:51:32 -0300

    Fix relay links

--- src/components/elements/Links/LinkRelayFeed.tsx
+++ src/components/elements/Links/LinkRelayFeed.tsx
@@ -1,17 +1,20 @@
+import { SxProps } from '@/components/ui/types'
 import { useRootStore } from '@/hooks/useRootStore'
 import { createRelayFeedModule } from '@/stores/modules/module.helpers'
 import type { LinkProps } from '@tanstack/react-router'
 import { Link, useRouter } from '@tanstack/react-router'
 import React, { useContext } from 'react'
+import { css } from 'react-strict-dom'
 import { DeckContext } from '../../modules/Deck/DeckContext'
 
 type Props = Omit<LinkProps, 'to' | 'search'> & {
   url: string
   children: React.ReactNode
+  sx?: SxProps
 }
 
 export const LinkRelayFeed = (props: Props) => {
-  const { url, target = '_self' } = props
+  const { url, target = '_self', sx } = props
   const root = useRootStore()
   const router = useRouter()
   const { index } = useContext(DeckContext)
@@ -20,6 +23,7 @@ export const LinkRelayFeed = (props: Props) => {
   if (isDeck) {
     return (
       <a
+        {...css.props(sx)}
         onClick={(e) => {
           root.decks.selected.add(createRelayFeedModule([url]), index + 1)
           e.preventDefault()
@@ -35,7 +39,8 @@ export const LinkRelayFeed = (props: Props) => {
       to={`/feed`}
       target={target}
       search={{ relay: url, kind: 1, limit: 50, type: 'relayfeed' }}
-      state={{ from: router.latestLocation.pathname } as never}>
+      state={{ from: router.latestLocation.pathname } as never}
+      {...css.props(sx)}>
       {props.children}
     </Link>
   )
