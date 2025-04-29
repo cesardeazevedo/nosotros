6364d59f44d78588cec1c8593660ea569a1bf7db
Cesar Augusto <cesardeazevedo@outlook.com>
2025-04-29 11:49:08 -0300

    Fix youtube posting crash

--- src/components/elements/Content/Youtube/YoutubeEmbed.tsx
+++ src/components/elements/Content/Youtube/YoutubeEmbed.tsx
@@ -1,11 +1,12 @@
 import { Button } from '@/components/ui/Button/Button'
 import { Text } from '@/components/ui/Text/Text'
+import { useGlobalSettings } from '@/hooks/useRootStore'
+import { palette } from '@/themes/palette.stylex'
 import { shape } from '@/themes/shape.stylex'
 import { spacing } from '@/themes/spacing.stylex'
 import { IconPlayerPlayFilled } from '@tabler/icons-react'
 import { useMemo, useState } from 'react'
 import { css, html } from 'react-strict-dom'
-import { Image } from '../Image/Image'
 import { ContentLink } from '../Link/Link'
 
 const REGEX_VIDEO_ID = /.*(?:youtu.be\/|v\/|u\/\w\/|shorts|embed\/|watch\?v=)([^#&?]*).*/
@@ -17,6 +18,7 @@ type Props = {
 export const YoutubeEmbed = (props: Props) => {
   const { src } = props
   const [open, setOpen] = useState(false)
+  const globalSettings = useGlobalSettings()
 
   const embedId = useMemo(() => src.match(REGEX_VIDEO_ID)?.[1].replace('/', ''), [src])
 
@@ -27,7 +29,13 @@ export const YoutubeEmbed = (props: Props) => {
     <html.div>
       {embedId ? (
         <html.div style={styles.content}>
-          {!open && <Image proxy={false} src={posterUrl} onClick={() => setOpen(true)} />}
+          {!open && (
+            <html.img
+              style={[styles.img]}
+              src={globalSettings.getImgProxyUrl('feed_img', posterUrl)}
+              onClick={() => setOpen(true)}
+            />
+          )}
           {open && <iframe {...css.props(styles.iframe)} src={iframeSrc} width={400} height={280} />}
           {!open && (
             <Button sx={styles.button} onClick={() => setOpen(true)}>
@@ -48,6 +56,21 @@ export const YoutubeEmbed = (props: Props) => {
 }
 
 const styles = css.create({
+  img: {
+    objectFit: 'cover',
+    width: '100%',
+    height: '100%',
+    maxWidth: '100%',
+    maxHeight: '100%',
+    userSelect: 'none',
+    cursor: 'pointer',
+    backgroundColor: palette.surfaceContainerLow,
+    borderRadius: shape.xl,
+    transition: 'transform 150ms ease',
+    ':active': {
+      transform: 'scale(0.985)',
+    },
+  },
   content: {
     position: 'relative',
     borderRadius: 1,
