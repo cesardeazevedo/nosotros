e2eb655c642bbae5fac33ba34c86efa430395f7a
Cesar Augusto <cesardeazevedo@outlook.com>
2025-05-07 15:04:41 -0300

    Small ui fix

--- src/components/elements/Layouts/HeaderBase.tsx
+++ src/components/elements/Layouts/HeaderBase.tsx
@@ -1,5 +1,6 @@
 import { Stack } from '@/components/ui/Stack/Stack'
 import { Text } from '@/components/ui/Text/Text'
+import type { SxProps } from '@/components/ui/types'
 import { spacing } from '@/themes/spacing.stylex'
 import React from 'react'
 import { css } from 'react-strict-dom'
@@ -8,12 +9,13 @@ export type Props = {
   label?: React.ReactNode
   leading?: React.ReactNode
   children?: React.ReactNode
+  sx?: SxProps
 }
 
 export const HeaderBase = (props: Props) => {
-  const { label, leading, children } = props
+  const { label, leading, children, sx } = props
   return (
-    <Stack gap={1} sx={styles.root} justify='space-between'>
+    <Stack gap={1} sx={[styles.root, sx]} justify='space-between'>
       {props.label ? (
         <Text variant='headline' size='sm'>
           {label}
