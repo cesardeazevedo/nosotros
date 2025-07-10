0bcda57f6e4280b666cb10c65fa4b67ed8f425b8
Cesar Augusto <cesardeazevedo@outlook.com>
2025-07-10 18:49:24 -0300

    Update search on submit instead of onChange
    feels more natural

--- src/components/modules/Search/hooks/useSearchChange.ts
+++ src/components/modules/Search/hooks/useSearchChange.ts
@@ -1,23 +1,19 @@
 import type { FeedStore } from '@/stores/feeds/feed.store'
 import { useNavigate } from '@tanstack/react-router'
-import { useObservableCallback, useSubscription } from 'observable-hooks'
-import { tap, throttleTime } from 'rxjs'
+import { useState } from 'react'
 
 export function useSearchChange(updateSearchParams?: boolean, feed?: FeedStore) {
   const navigate = useNavigate()
-  const [onChange, query$] = useObservableCallback<string>((input$) => {
-    return input$.pipe(
-      throttleTime(1300, undefined, { leading: false, trailing: true }),
-      tap((query) => {
-        if (updateSearchParams) {
-          navigate({ to: '/search', search: { q: query } })
-        }
-        if (feed) {
-          feed.setFilter({ ...feed.filter, search: query })
-        }
-      }),
-    )
-  })
-  useSubscription(query$)
-  return onChange
+  const [query, setQuery] = useState(feed?.filter.search)
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setQuery(e.target.value)
+  }
+  const onSubmit = () => {
+    feed?.setFilter({ ...feed.filter, search: query })
+    if (updateSearchParams) {
+      navigate({ to: '/search', search: { q: query } })
+    }
+  }
+
+  return { query, onSubmit, onChange }
 }
