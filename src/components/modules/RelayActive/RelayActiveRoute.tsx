7264b577f3e4bd25d3013c3f4cfcb711fecfc8b1
Cesar Augusto <cesardeazevedo@outlook.com>
2025-07-10 18:59:44 -0300

    Reset scroll position on RelayActiveRoute

--- src/components/modules/RelayActive/RelayActiveRoute.tsx
+++ src/components/modules/RelayActive/RelayActiveRoute.tsx
@@ -1,11 +1,13 @@
 import { RouteContainer } from '@/components/elements/Layouts/RouteContainer'
 import { RelayActiveHeader } from '@/components/modules/RelayActive/RelayActiveHeader'
 import { useMobile } from '@/hooks/useMobile'
+import { useResetScroll } from '@/hooks/useResetScroll'
 import { RelayActiveList } from './RelayActiveList'
 import { RelayActiveTable } from './RelayActiveTable'
 
 export const RelayActiveRoute = () => {
   const isMobile = useMobile()
+  useResetScroll()
   return (
     <RouteContainer maxWidth='lg' header={<RelayActiveHeader />}>
       {isMobile ? <RelayActiveList /> : <RelayActiveTable />}
