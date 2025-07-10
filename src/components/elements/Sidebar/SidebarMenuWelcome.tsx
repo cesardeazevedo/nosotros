0cbf64eb2106a7358bba1ee83c4d12fd8369c807
Cesar Augusto <cesardeazevedo@outlook.com>
2025-07-10 16:06:29 -0300

    Add deck menu to the welcome sidebar

--- src/components/elements/Sidebar/SidebarMenuWelcome.tsx
+++ src/components/elements/Sidebar/SidebarMenuWelcome.tsx
@@ -1,18 +1,19 @@
 import { Button } from '@/components/ui/Button/Button'
-import { listItemTokens } from '@/components/ui/ListItem/ListItem.stylex'
+import { Divider } from '@/components/ui/Divider/Divider'
 import { MenuItem } from '@/components/ui/MenuItem/MenuItem'
-import { MenuList } from '@/components/ui/MenuList/MenuList'
+import { Stack } from '@/components/ui/Stack/Stack'
+import { useMobile } from '@/hooks/useMobile'
 import { shape } from '@/themes/shape.stylex'
-import { spacing } from '@/themes/spacing.stylex'
 import { IconBell, IconListDetails, IconNews, IconPhoto, IconPhotoFilled, IconSettings } from '@tabler/icons-react'
 import { Link } from '@tanstack/react-router'
 import { useContext } from 'react'
-import { css } from 'react-strict-dom'
+import { css, html } from 'react-strict-dom'
 import { IconHome } from '../Icons/IconHome'
 import { IconHomeFilled } from '../Icons/IconHomeFilled'
 import { IconNostr } from '../Icons/IconNostr'
 import { LinkSignIn } from '../Links/LinkSignIn'
 import { SidebarContext } from './SidebarContext'
+import { SidebarMenuDecks } from './SidebarMenuDecks'
 import { SidebarMenuRelays } from './SidebarMenuRelays'
 
 const iconProps = {
@@ -22,55 +23,68 @@ const iconProps = {
 
 export const SidebarMenuWelcome = () => {
   const context = useContext(SidebarContext)
+  const isMobile = useMobile()
   return (
-    <MenuList elevation={0} sx={styles.root}>
-      <LinkSignIn>
-        <Button sx={styles.button} fullWidth variant='filled' icon={<IconNostr />}>
-          Join Nostr
-        </Button>
-      </LinkSignIn>
-      <br />
-      <Link to='/'>
-        {({ isActive }) => (
-          <MenuItem
-            selected={isActive}
-            onClick={() => context.setPane(false)}
-            leadingIcon={isActive ? <IconHomeFilled {...iconProps} /> : <IconHome {...iconProps} />}
-            label='Home'
-          />
-        )}
-      </Link>
-      <Link to='/media'>
-        {({ isActive }) => (
-          <MenuItem
-            selected={isActive}
-            onClick={() => context.setPane(false)}
-            leadingIcon={isActive ? <IconPhotoFilled {...iconProps} /> : <IconPhoto {...iconProps} />}
-            label='Media'
-          />
-        )}
-      </Link>
-      <Link to='/articles'>
-        {({ isActive }) => (
-          <MenuItem
-            selected={isActive}
-            onClick={() => context.setPane(false)}
-            leadingIcon={<IconNews {...iconProps} />}
-            label='Articles'
-          />
-        )}
-      </Link>
-      <MenuItem disabled leadingIcon={<IconBell {...iconProps} />} label='Notifications' />
-      <MenuItem
-        leadingIcon={<IconListDetails {...iconProps} />}
-        label='Lists'
-        onClick={() => context.setPane('/lists')}
-      />
-      <SidebarMenuRelays />
-      <Link to='/settings'>
-        <MenuItem onClick={() => {}} leadingIcon={<IconSettings {...iconProps} />} label='Settings' />
-      </Link>
-    </MenuList>
+    <Stack horizontal={false} sx={styles.root} gap={1}>
+      <Stack horizontal={false} gap={0.5} sx={styles.wrapper}>
+        <LinkSignIn>
+          <Button sx={styles.button} fullWidth variant='filled' icon={<IconNostr />}>
+            Join Nostr
+          </Button>
+        </LinkSignIn>
+        <br />
+        <Link to='/'>
+          {({ isActive }) => (
+            <MenuItem
+              selected={isActive}
+              onClick={() => context.setPane(false)}
+              leadingIcon={isActive ? <IconHomeFilled {...iconProps} /> : <IconHome {...iconProps} />}
+              label='Home'
+            />
+          )}
+        </Link>
+        <Link to='/media'>
+          {({ isActive }) => (
+            <MenuItem
+              selected={isActive}
+              onClick={() => context.setPane(false)}
+              leadingIcon={isActive ? <IconPhotoFilled {...iconProps} /> : <IconPhoto {...iconProps} />}
+              label='Media'
+            />
+          )}
+        </Link>
+        <Link to='/articles'>
+          {({ isActive }) => (
+            <MenuItem
+              selected={isActive}
+              onClick={() => context.setPane(false)}
+              leadingIcon={<IconNews {...iconProps} />}
+              label='Articles'
+            />
+          )}
+        </Link>
+        <MenuItem disabled leadingIcon={<IconBell {...iconProps} />} label='Notifications' />
+        <MenuItem
+          leadingIcon={<IconListDetails {...iconProps} />}
+          label='Lists'
+          onClick={() => context.setPane('/lists')}
+        />
+      </Stack>
+      {!isMobile && (
+        <>
+          <Divider />
+          <html.div style={styles.wrapper}>
+            <SidebarMenuDecks expanded />
+          </html.div>
+        </>
+      )}
+      <Stack horizontal={false} gap={0.5} sx={styles.wrapper}>
+        <SidebarMenuRelays />
+        <Link to='/settings'>
+          <MenuItem onClick={() => {}} leadingIcon={<IconSettings {...iconProps} />} label='Settings' />
+        </Link>
+      </Stack>
+    </Stack>
   )
 }
 
@@ -79,8 +93,10 @@ const styles = css.create({
     width: '100%',
     borderRadius: shape.lg,
     backgroundColor: 'transparent',
-    [listItemTokens.containerMinHeight$sm]: 50,
-    [listItemTokens.leadingSpace]: spacing.padding3,
+  },
+  wrapper: {
+    width: '100%',
+    paddingInline: 12,
   },
   button: {
     height: 50,
