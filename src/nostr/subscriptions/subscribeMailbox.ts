caa9befb04558dc8c746fbec36cf84b36de11961
Cesar Augusto <cesardeazevedo@outlook.com>
2025-07-10 18:03:44 -0300

    Add fallback relays if users don't have relays setup
    This should had been there already

--- src/nostr/subscriptions/subscribeMailbox.ts
+++ src/nostr/subscriptions/subscribeMailbox.ts
@@ -1,13 +1,22 @@
+import { FALLBACK_RELAYS } from '@/constants/relays'
 import { pickBy } from '@/core/helpers/pickBy'
-import { combineLatestWith, EMPTY, map, mergeMap, of, take } from 'rxjs'
+import { combineLatestWith, defaultIfEmpty, EMPTY, map, mergeMap, of, take } from 'rxjs'
 import type { NostrContext } from '../context'
 import type { RelayStatsDBMap } from '../db/queryRelayStats'
 import { relayStats } from '../db/queryRelayStats'
 import { selectRelays } from '../helpers/selectRelays'
 import { ShareReplayCache } from '../replay'
 import type { UserRelay } from '../types'
-import { metadataSymbol } from '../types'
+import { metadataSymbol, READ, WRITE } from '../types'
 import { subscribeRelayList } from './subscribeRelayList'
+
+const fallbackUserRelays = FALLBACK_RELAYS.map(
+  (relay) =>
+    ({
+      relay,
+      permission: READ | WRITE,
+    }) as UserRelay,
+)
 
 export const toArrayRelay = map((data: UserRelay[]) => data.map((x) => x.relay))
 
@@ -19,6 +28,7 @@ const selectRelaysReplay = replay.wrap((_key: string, data: UserRelay[], ctx: No
 export const subscribeMailbox = (pubkey: string, ctx: NostrContext) => {
   return subscribeRelayList(pubkey, ctx).pipe(
     map((event) => event[metadataSymbol].relayList || []),
+    defaultIfEmpty(fallbackUserRelays),
     combineLatestWith(relayStats.pipe(take(1))),
     map(([userRelays, relayStats]) => {
       return [
