9b4baae2addeb4c194533d5f3ff785ea9729c3fe
Cesar Augusto <cesardeazevedo@outlook.com>
2025-07-13 14:33:25 -0300

    Auto play video fixes

--- src/stores/media/media.store.ts
+++ src/stores/media/media.store.ts
@@ -3,6 +3,7 @@ import { makeAutoObservable, observable } from 'mobx'
 class MediaStore {
   dims = new Map<string, [number, number]>()
   errors = observable.set<string>()
+  currentVideo: HTMLVideoElement | null = null
 
   constructor() {
     makeAutoObservable(this)
@@ -18,6 +19,22 @@ class MediaStore {
 
   addError(src: string) {
     this.errors.add(src)
+  }
+
+  setVideo(video: HTMLVideoElement, autoPlay: boolean) {
+    if (this.currentVideo && this.currentVideo !== video) {
+      this.currentVideo.pause()
+    }
+    this.currentVideo = video
+    if (autoPlay) {
+      this.currentVideo.play().catch(() => {})
+    }
+  }
+
+  removeVideo(video: HTMLVideoElement) {
+    if (this.currentVideo === video) {
+      this.currentVideo = null
+    }
   }
 }
 
