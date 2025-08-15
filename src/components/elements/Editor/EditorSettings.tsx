c1dfdc1ce66628ad33b6bfe7b4ce60959fbfd36a
Cesar Augusto <cesardeazevedo@outlook.com>
2025-08-15 15:41:08 -0300

    Refactor on editor components

--- src/components/elements/Editor/EditorSettings.tsx
+++ src/components/elements/Editor/EditorSettings.tsx
@@ -1,82 +1,92 @@
 import { buttonTokens } from '@/components/ui/Button/Button.stylex'
-import { IconButton } from '@/components/ui/IconButton/IconButton'
 import { Stack } from '@/components/ui/Stack/Stack'
 import { Switch } from '@/components/ui/Switch/Switch'
 import { Text } from '@/components/ui/Text/Text'
-import { Tooltip } from '@/components/ui/Tooltip/Tooltip'
-import type { EditorStore } from '@/stores/editor/editor.store'
-import { shape } from '@/themes/shape.stylex'
+import { useSettings, useToggleSettings } from '@/hooks/useSettings'
+import { palette } from '@/themes/palette.stylex'
 import { spacing } from '@/themes/spacing.stylex'
-import { IconCode } from '@tabler/icons-react'
+import { IconBraces } from '@tabler/icons-react'
 import { githubDarkTheme } from '@uiw/react-json-view/githubDark'
-import { observer } from 'mobx-react-lite'
-import { lazy, Suspense, useState } from 'react'
+import { lazy, memo, Suspense, useState } from 'react'
 import { css } from 'react-strict-dom'
 import { EditorSettingsUpload } from './EditorSettingsUpload'
-
-type Props = {
-  store: EditorStore
-}
+import { useEditorSelector } from './hooks/useEditor'
 
 const jsonTheme = {
   ...githubDarkTheme,
-  '--w-rjv-background-color': '#0d0f12',
+  '--w-rjv-background-color': '#000',
+  margin: 1,
 }
 
 const JsonView = lazy(() => import('@uiw/react-json-view'))
 
-export const EditorSettings = observer(function EditorSettings(props: Props) {
+const EditorJsonView = () => {
+  const event = useEditorSelector((editor) => editor.event)
+  return (
+    event && (
+      <JsonView
+        highlightUpdates={false}
+        displayDataTypes={false}
+        value={event}
+        {...css.props(styles.json)}
+        style={{ ...jsonTheme }}
+      />
+    )
+  )
+}
+
+export const EditorSettings = memo(function EditorSettings() {
   const [jsonOpen, setJsonOpen] = useState(false)
-  const { store } = props
+  const toggleSettings = useToggleSettings()
+  const toggle = useEditorSelector((editor) => editor.toggle)
+  const settings = useSettings()
+  const nsfwEnabled = useEditorSelector((editor) => editor.nsfwEnabled)
 
   return (
     <>
-      <Stack horizontal={false} gap={1} sx={styles.root} align='stretch' justify='flex-start'>
-        <Stack horizontal={false} sx={styles.content} gap={2} align='flex-start' justify='flex-start'>
-          <Stack gap={1}>
+      <Stack horizontal={false} gap={1} sx={styles.root} align='stretch' justify='space-between'>
+        <Stack horizontal={false} sx={styles.content} gap={2} align='stretch' justify='space-between'>
+          <Stack grow gap={1} align='stretch' justify='space-between'>
             <Text variant='title' size='lg'>
               Options
             </Text>
-            <Tooltip text='See Raw Event'>
-              <IconButton toggle={jsonOpen} size='sm' onClick={() => setJsonOpen((prev) => !prev)}>
-                <IconCode strokeWidth='2.0' size={18} />
-              </IconButton>
-            </Tooltip>
           </Stack>
-          <Stack horizontal={false} gap={2}>
-            <Stack gap={1}>
-              <Text size='md' sx={styles.label}>
-                Upload servers
-              </Text>
-              <EditorSettingsUpload store={store} />
-            </Stack>
-            <Stack gap={1}>
-              <Text size='md' sx={styles.label}>
+          <Stack grow gap={1} align='stretch' justify='space-between'>
+            <Text size='lg' sx={styles.label}>
+              Upload servers
+            </Text>
+            <EditorSettingsUpload />
+          </Stack>
+          <Stack grow gap={1} align='stretch' justify='space-between'>
+            <Stack horizontal={false}>
+              <Text size='lg' sx={styles.label}>
                 Client tag
               </Text>
-              <Switch checked={store.clientEnabled.value} onChange={() => store.clientEnabled.toggle()} />
-            </Stack>
-            <Text size='sm' sx={styles.description}>
-              Client tag will let other users know you are using nosotros
-            </Text>
-            <Stack gap={1}>
-              <Text size='md' sx={styles.label}>
-                NSFW
+              <Text size='md' sx={styles.description}>
+                Client tag will let other users know you are using nosotros
               </Text>
-              <Switch checked={store.nsfwEnabled.value} onChange={() => store.nsfwEnabled.toggle()} />
             </Stack>
+            <Switch checked={settings.clientTag} onChange={() => toggleSettings('clientTag')} />
+          </Stack>
+          <Stack grow gap={1} align='stretch' justify='space-between'>
+            <Text size='lg' sx={styles.label}>
+              NSFW
+            </Text>
+            <Switch checked={nsfwEnabled} onChange={() => toggle('nsfwEnabled')} />
+          </Stack>
+          <Stack grow gap={1} align='stretch' justify='space-between'>
+            <Text size='lg' sx={styles.label}>
+              <Stack gap={1}>
+                Show raw event <IconBraces strokeWidth='2.0' size={18} />
+              </Stack>
+            </Text>
+            <Switch checked={jsonOpen} onChange={() => setJsonOpen((prev) => !prev)} />
           </Stack>
         </Stack>
         <Stack>
           {jsonOpen && (
             <Suspense>
-              <JsonView
-                highlightUpdates={false}
-                displayDataTypes={false}
-                value={store.event}
-                {...css.props(styles.json)}
-                style={{ ...jsonTheme }}
-              />
+              <EditorJsonView />
             </Suspense>
           )}
         </Stack>
@@ -90,10 +100,9 @@ const styles = css.create({
     width: '100%',
     position: 'relative',
     overflow: 'hidden',
-    borderBottomLeftRadius: shape.xl,
-    borderBottomRightRadius: shape.xl,
   },
   content: {
+    width: '100%',
     paddingBlock: spacing.padding2,
     paddingInline: spacing.padding3,
   },
@@ -113,6 +122,7 @@ const styles = css.create({
     fontWeight: 500,
   },
   description: {
-    maxWidth: 240,
+    color: palette.onSurfaceVariant,
+    maxWidth: 300,
   },
 })
--- src/components/elements/Editor/Toolbar/EditorButtonAddMedia.tsx
+++ src/components/elements/Editor/Toolbar/EditorButtonAddMedia.tsx
@@ -2,24 +2,24 @@ import { useContentContext } from '@/components/providers/ContentProvider'
 import type { Props as IconButtonProps } from '@/components/ui/IconButton/IconButton'
 import { IconButton } from '@/components/ui/IconButton/IconButton'
 import { Tooltip } from '@/components/ui/Tooltip/Tooltip'
-import type { EditorStore } from '@/stores/editor/editor.store'
 import { IconPhotoPlus } from '@tabler/icons-react'
+import { memo } from 'react'
+import { useEditorSelector } from '../hooks/useEditor'
 
-type Props = IconButtonProps & {
-  store: EditorStore
-}
+type Props = IconButtonProps & {}
 
-export const EditorButtonAddMedia = function EditorButtonAddMedia(props: Props) {
-  const { store, ...rest } = props
+export const EditorButtonAddMedia = memo(function EditorButtonAddMedia(props: Props) {
+  const { ...rest } = props
+  const selectFiles = useEditorSelector((editor) => editor.selectFiles)
   const { dense } = useContentContext()
   return (
     <Tooltip cursor='arrow' text='Add Media' enterDelay={200}>
       <IconButton
         size={dense ? 'sm' : 'md'}
         icon={<IconPhotoPlus size={dense ? 20 : 22} strokeWidth='1.6' />}
-        onClick={store.selectFiles}
+        onClick={selectFiles}
         {...rest}
       />
     </Tooltip>
   )
-}
+})
