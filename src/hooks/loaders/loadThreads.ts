be0c9e6f3a7fb140b3a94acbba9cf19b32ac65c1
Cesar Augusto <cesardeazevedo@outlook.com>
2025-08-19 00:30:08 -0300

    Fixes on thread loader

--- src/hooks/loaders/loadThreads.ts
+++ src/hooks/loaders/loadThreads.ts
@@ -1,10 +1,16 @@
 import type { QueryClient } from '@tanstack/react-query'
 import { eventFromNIP19QueryOptions } from '../query/useQueryBase'
-import { eventRepliesQueryOptions } from '../query/useReplies'
+import { eventRepliesByIdQueryOptions, eventRepliesQueryOptions } from '../query/useReplies'
 
 export async function loadThreads(queryClient: QueryClient, nip19: string) {
   const events = await queryClient.fetchQuery(eventFromNIP19QueryOptions(nip19))
-  if (events[0]) {
-    await queryClient.fetchQuery(eventRepliesQueryOptions(events[0]))
+  const event = events[0]
+  if (event) {
+    const rootId = event.metadata?.rootId
+    if (!rootId) {
+      await queryClient.fetchQuery(eventRepliesQueryOptions(event))
+    } else {
+      await queryClient.fetchQuery(eventRepliesByIdQueryOptions(rootId))
+    }
   }
 }
