44d69ece1cc2e194736dd5b04bd73780f19dce11
Cesar Augusto <cesardeazevedo@outlook.com>
2025-08-20 16:54:40 -0300

    Escape image source on image proxy url
    We were missing parameters of some urls

--- src/utils/imgproxy.ts
+++ src/utils/imgproxy.ts
@@ -3,5 +3,5 @@ const IMGPROXY_URL = import.meta.env.VITE_IMGPROXY_URL
 type ImgProxyPreset = 'feed_img' | 'user_avatar' | 'high_res'
 
 export function getImgProxyUrl(preset: ImgProxyPreset, src: string) {
-  return `${IMGPROXY_URL}/_/${preset}/plain/${src}`
+  return `${IMGPROXY_URL}/_/${preset}/plain/${encodeURIComponent(src)}`
 }
