d4277b3726a2b35d12bdcb49bc13987db534d5cc
Cesar Augusto <cesardeazevedo@outlook.com>
2025-08-26 19:30:36 -0300

    Fix some of replies loading for comments

--- src/hooks/loaders/loadThreads.ts
+++ src/hooks/loaders/loadThreads.ts
@@ -1,16 +1,11 @@
 import type { QueryClient } from '@tanstack/react-query'
 import { eventFromNIP19QueryOptions } from '../query/useQueryBase'
-import { eventRepliesByIdQueryOptions, eventRepliesQueryOptions } from '../query/useReplies'
+import { eventRepliesQueryOptions } from '../query/useReplies'
 
 export async function loadThreads(queryClient: QueryClient, nip19: string) {
   const events = await queryClient.fetchQuery(eventFromNIP19QueryOptions(nip19))
   const event = events[0]
   if (event) {
-    const rootId = event.metadata?.rootId
-    if (!rootId) {
-      await queryClient.fetchQuery(eventRepliesQueryOptions(event))
-    } else {
-      await queryClient.fetchQuery(eventRepliesByIdQueryOptions(rootId))
-    }
+    await queryClient.fetchQuery(eventRepliesQueryOptions(event))
   }
 }
