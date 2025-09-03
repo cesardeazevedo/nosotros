218d8c6138b457b9f217be89ab8fec5fbc22ece2
Cesar Augusto <cesardeazevedo@outlook.com>
2025-09-03 16:58:11 -0300

    Small improvements on starter pack list in different resolutions

--- src/components/modules/Lists/StarterPacks/StarterPackList.tsx
+++ src/components/modules/Lists/StarterPacks/StarterPackList.tsx
@@ -2,11 +2,12 @@ import { Kind } from '@/constants/kinds'
 import { createListModule } from '@/hooks/modules/createListModule'
 import { useFeedState } from '@/hooks/state/useFeed'
 import { useCurrentPubkey } from '@/hooks/useAuth'
+import { useLG, useMobile } from '@/hooks/useMobile'
+import { spacing } from '@/themes/spacing.stylex'
 import { memo } from 'react'
 import { css, html } from 'react-strict-dom'
 import { Feed } from '../../Feed/Feed'
 import { StarterPackCard } from './StarterPackCard'
-import { spacing } from '@/themes/spacing.stylex'
 
 type Props = {
   dense?: boolean
@@ -14,11 +15,17 @@ type Props = {
 
 export const StarterPackList = memo(function StarterPackList(props: Props) {
   const { dense } = props
+  const isLG = useLG()
+  const isMobile = useMobile()
   const pubkey = useCurrentPubkey()
   const feed = useFeedState(createListModule(Kind.StarterPack, pubkey))
   return (
     <Feed
-      wrapper={(children) => <html.div style={[styles.root, dense && styles.root$dense]}>{children}</html.div>}
+      wrapper={(children) => (
+        <html.div style={[styles.root, isLG && styles.root$lg, (dense || isMobile) && styles.root$dense]}>
+          {children}
+        </html.div>
+      )}
       feed={feed}
       render={(event) => <StarterPackCard event={event} />}
     />
@@ -30,6 +37,9 @@ const styles = css.create({
     display: 'grid',
     gridTemplateColumns: 'repeat(3, minmax(0, 3fr))',
     padding: spacing.padding1,
+  },
+  root$lg: {
+    gridTemplateColumns: 'repeat(2, minmax(0, 3fr))',
   },
   root$dense: {
     gridTemplateColumns: 'repeat(1, minmax(0, 3fr))',
