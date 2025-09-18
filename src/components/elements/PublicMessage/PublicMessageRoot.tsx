13daec8389898ab3a3341d01fc80b1b16055a52a
Cesar Augusto <cesardeazevedo@outlook.com>
2025-09-18 03:43:55 -0300

    Better header UI on PublicMessageRoot component

--- src/components/elements/PublicMessage/PublicMessageRoot.tsx
+++ src/components/elements/PublicMessage/PublicMessageRoot.tsx
@@ -1,11 +1,17 @@
 import { NoteProvider } from '@/components/providers/NoteProvider'
 import { Divider } from '@/components/ui/Divider/Divider'
+import { Stack } from '@/components/ui/Stack/Stack'
 import type { NostrEventDB } from '@/db/sqlite/sqlite.types'
 import { useNoteState } from '@/hooks/state/useNote'
+import { useEventTag } from '@/hooks/useEventUtils'
+import { spacing } from '@/themes/spacing.stylex'
+import { IconChevronRight } from '@tabler/icons-react'
+import { css } from 'react-strict-dom'
 import { PostActions } from '../Posts/PostActions/PostActions'
 import { PostContent } from '../Posts/PostContent'
-import { PostHeader } from '../Posts/PostHeader'
+import { PostUserHeader } from '../Posts/PostUserHeader'
 import { Replies } from '../Replies/Replies'
+import { UserHeader } from '../User/UserHeader'
 
 type Props = {
   event: NostrEventDB
@@ -13,10 +19,15 @@ type Props = {
 
 export const PublicMessageRoot = (props: Props) => {
   const { event } = props
+  const to = useEventTag(event, 'p')
   const note = useNoteState(event, { repliesOpen: true, forceSync: true })
   return (
     <NoteProvider value={{ event }}>
-      <PostHeader event={event} />
+      <Stack sx={styles.header} align='center' gap={1}>
+        <PostUserHeader event={event} />
+        <IconChevronRight size={16} strokeWidth='3' />
+        {to && <UserHeader pubkey={to} />}
+      </Stack>
       <PostContent note={note} />
       <PostActions renderReply={false} note={note} />
       {note.state.repliesOpen && (
@@ -28,3 +39,9 @@ export const PublicMessageRoot = (props: Props) => {
     </NoteProvider>
   )
 }
+
+const styles = css.create({
+  header: {
+    padding: spacing.padding2,
+  },
+})
