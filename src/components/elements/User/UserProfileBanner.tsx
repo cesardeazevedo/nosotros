02cf1a8f5339cee835dce36615dca49521f1a7b2
Cesar Augusto <cesardeazevedo@outlook.com>
2025-10-11 18:22:41 -0300

    Render UserProfileBanner component on ProfilerPopover
    Decluttering duplication

--- src/components/elements/User/UserProfileBanner.tsx
+++ src/components/elements/User/UserProfileBanner.tsx
@@ -1,5 +1,6 @@
 import { openImageDialogAtom } from '@/atoms/dialog.atoms'
 import { addMediaErrorAtom, mediaErrorsAtom } from '@/atoms/media.atoms'
+import type { SxProps } from '@/components/ui/types'
 import { useUserState } from '@/hooks/state/useUser'
 import { palette } from '@/themes/palette.stylex'
 import { getImgProxyUrl } from '@/utils/imgproxy'
@@ -9,10 +10,11 @@ import { css, html } from 'react-strict-dom'
 type Props = {
   pubkey: string
   dense?: boolean
+  sx?: SxProps
 }
 
 export const UserProfileBanner = function UserProfileBanner(props: Props) {
-  const { pubkey, dense } = props
+  const { pubkey, dense, sx } = props
   const user = useUserState(pubkey)
   const { banner } = user?.metadata || {}
   const hasError = useAtomValue(mediaErrorsAtom).has(banner || '')
@@ -23,7 +25,7 @@ export const UserProfileBanner = function UserProfileBanner(props: Props) {
     return (
       <html.img
         src={getImgProxyUrl('high_res', banner)}
-        style={[styles.root, dense && styles.root$dense]}
+        style={[styles.root, dense && styles.root$dense, sx]}
         onClick={() => pushImage({ src: banner })}
         onError={() => addError(banner)}
       />
