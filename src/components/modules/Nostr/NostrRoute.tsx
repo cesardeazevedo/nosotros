56a86a2ca4ad53a6473f00d9d4ff2dda59fe0fe7
Cesar Augusto <cesardeazevedo@outlook.com>
2025-11-05 01:48:51 -0300

    Invalidate replies query when navigating to NostrRoute

--- src/components/modules/Nostr/NostrRoute.tsx
+++ src/components/modules/Nostr/NostrRoute.tsx
@@ -1,6 +1,11 @@
+import { Kind } from '@/constants/kinds'
+import { queryKeys } from '@/hooks/query/queryKeys'
+import { addressFromNIP19 } from '@/utils/nip19'
+import { useQueryClient } from '@tanstack/react-query'
+import type { DecodedResult } from 'nostr-tools/nip19'
+import { useEffect } from 'react'
 import { NostrEventRoute } from '../NostrEvent/NostrEventRoute'
 import { NProfileRoute } from '../NProfile/NProfileRoute'
-import type { DecodedResult } from 'nostr-tools/nip19'
 import { NProfileRouteMasked } from '../NProfile/NProfileRouteMasked'
 
 type Props = {
@@ -12,6 +17,32 @@ type Props = {
 // Selects which Nostr route to render based on the decoded NIP-19 type
 export const NostrRoute = (props: Props) => {
   const { decoded, nostr, masked } = props
+  const queryClient = useQueryClient()
+
+  useEffect(() => {
+    switch (decoded?.type) {
+      case 'note': {
+        queryClient.invalidateQueries({
+          queryKey: queryKeys.tag('e', [decoded.data], Kind.Text),
+        })
+        break
+      }
+      case 'nevent': {
+        queryClient.invalidateQueries({
+          queryKey: queryKeys.tag('e', [decoded.data.id], Kind.Text),
+        })
+        break
+      }
+      case 'naddr': {
+        const address = addressFromNIP19(decoded.data)
+        queryClient.invalidateQueries({
+          queryKey: queryKeys.tag('a', [address], Kind.Text),
+        })
+        break
+      }
+    }
+  }, [decoded, nostr])
+
   switch (decoded?.type) {
     case 'npub':
       return <NProfileRoute pubkey={decoded.data} />
