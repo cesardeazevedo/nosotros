6cd47bd2d21772d9cb9eda041c322d0b4aecdba5
Cesar Augusto <cesardeazevedo@outlook.com>
2025-11-04 21:17:22 -0300

    Fix post link active state and add note to a new tab with metaKey

--- src/hooks/useNavigations.ts
+++ src/hooks/useNavigations.ts
@@ -38,6 +38,10 @@ export function useIsCurrentRouteEventID(id: string) {
       case 'nprofile': {
         return context.decoded.data.pubkey === id
       }
+      default: {
+        return false
+      }
     }
   }
+  return false
 }
