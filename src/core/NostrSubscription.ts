d6d1f0c1e6f80dfcd6d3526772546cfa733e66de
Cesar Augusto <cesardeazevedo@outlook.com>
2026-02-08 19:52:16 -0300

    Fix unused events map on NostrSubscription

--- src/core/NostrSubscription.ts
+++ src/core/NostrSubscription.ts
@@ -1,4 +1,3 @@
-import type { NostrEvent } from 'nostr-tools'
 import { createFilter } from './helpers/createFilter'
 import { makeSubId } from './helpers/kindsToId'
 import type { NostrFilter } from './types'
@@ -7,17 +6,14 @@ export type RelayFilters = [string, NostrFilter]
 
 export type SubscriptionOptions = {
   id?: string
-  events?: Map<string, { id: string; created_at: number }>
 }
 
 export class NostrSubscription {
   readonly id: string
   readonly filter: NostrFilter
-  events: Map<string, NostrEvent>
 
   constructor(filter: NostrFilter, options?: SubscriptionOptions) {
     this.id = makeSubId(options?.id, filter.kinds)
     this.filter = createFilter(filter)
-    this.events = options?.events || new Map()
   }
 }
