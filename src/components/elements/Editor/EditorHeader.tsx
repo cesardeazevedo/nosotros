74a4980f10bcaa3561cf196dd95be62c8a03ab34
Cesar Augusto <cesardeazevedo@outlook.com>
2026-02-22 04:29:51 -0300

    Removes framer-motion from EditorHeader

--- src/components/elements/Editor/EditorHeader.tsx
+++ src/components/elements/Editor/EditorHeader.tsx
@@ -2,7 +2,6 @@ import { useContentContext } from '@/components/providers/ContentProvider'
 import { Stack } from '@/components/ui/Stack/Stack'
 import { Text } from '@/components/ui/Text/Text'
 import { useCurrentPubkey } from '@/hooks/useAuth'
-import { motion } from 'framer-motion'
 import { memo } from 'react'
 import { LinkSignIn } from '../Links/LinkSignIn'
 import { UserName } from '../User/UserName'
@@ -17,13 +16,7 @@ export const EditorHeader = memo(function EditorHeader() {
     !dense && (
       <>
         {pubkey ? (
-          <motion.div
-            key='username'
-            initial={{ translateY: -6, opacity: 0 }}
-            animate={{ translateY: -1, opacity: 1 }}
-            exit={{ translateY: -6, opacity: 0 }}>
-            <UserName pubkey={pubkey} />
-          </motion.div>
+          <UserName pubkey={pubkey} />
         ) : (
           <LinkSignIn>
             <Stack gap={1}>
